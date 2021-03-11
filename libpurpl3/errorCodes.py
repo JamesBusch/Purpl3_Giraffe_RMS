da5e4f26c8c6942c6bd4655fbf735cffe9022db5
qawse3dr <lawrencemilne38@gmail.com>
2021-03-11 00:27:50 -0500

    ReqID:8 connect though ssh with key     Computer is now able to connect though ssh with a given key set in config files     unit tests are also added with assumption that they will be done on root@localhost

--- libpurpl3/errorCodes.py
+++ libpurpl3/errorCodes.py
@@ -91,10 +91,9 @@ def getErrorCodeList() -> dict:
     getAttrName(ERROR_FILE_NOT_FOUND): Error(3,"Returned: %d, file %s was not found"),
     
     getAttrName(ERROR_ATTRIBUTE_NOT_FOUND) : Error(20,"Returned: %d, Attribute %s could not be found."),
-
+    
     #46-59 ssh errors
     getAttrName(ERROR_CONNECTION_FAILED): Error(46,"Returned: %d, Connection to %s@%s could not be made."),
-
     getAttrName(ERROR_CANT_FIND_SSH_KEY) : Error(49,"Returned %d: failed to find a valid ssh key at %s."),
 
     getAttrName(ERROR_SSH_AUTHENTICATION_FAILED) : Error(50,"Returned %d: failed authentication over ssh for %s@%s."),
