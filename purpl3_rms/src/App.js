ff0939da09e7b1940839e143a2f464ccd9b822bc
soory <soory.ranga@gmail.com>
2021-03-15 11:50:05 -0400

    adding the run script page and how to commit and rebase and stuff

--- purpl3_rms/src/App.js
+++ purpl3_rms/src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
-
+import Run_script_page from './Components/run_script_page/run_script_page_soory';
 function App() {
   return (
     <div className="App">
-      
-      <button onClick={Ping}>
-          Ping
-      </button>
+      <Run_script_page/>
     </div>
   );
 }
 
-function Ping(){
-  
-  axios.post("/ping", {
-    body: {ping:"ping"}
-    }).then((res) => {
-      alert(JSON.stringify(res.data))
-    }).catch((res) =>{
-      alert("Post Failed")
-    })
-}
 export default App;
---
+++ purpl3_rms/src/Components/table/table_soory.js
@@ -0,0 +1,20 @@
+import './table.css'
+import Button from './button/button_soory';
+
+const Table = (props) => {
+    const comp = this;
+    console.log(comp);
+    return (
+        <div id="table">
+            {addButtons(props)}
+        </div>
+    )
+}
+function addButtons(params) {
+    console.log(params);
+    let output = params.input.map(item=> <Button name={item.name} func={item.script}/>)
+    
+    return <div>{output}</div>
+}
+
+export default Table
